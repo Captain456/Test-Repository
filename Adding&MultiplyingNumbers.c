ae956e5745faa1b867f7f0c0676ca747347927f1
Captain456 <Christopher_Gibbs@student.uml.edu>
2015-06-03 12:37:09 -0400

    Renamed 'AddingNumbers.c' to 'Adding&MultiplyingNumbers.c' and included a multiplication section to the C file.

--- Adding&MultiplyingNumbers.c
+++ Adding&MultiplyingNumbers.c
@@ -2,12 +2,14 @@
 
 int main(void)
 {
-	int i, j, k;
+	int i, j, k, m;
 	i = 10;
 	printf("i is equal to: %d\n", i);
 	j = 9 + i;
 	printf("j is equal to 9 + i. j is equal to: %d\n", j);
 	k = j + i + 1;
 	printf("k is equal to 1 + j + i. k is equal to: %d\n", k);
+	m = k * j * i;
+	printf("m stands for Multiplication! It's equal to i * j * k. m is equal to: %d\n", m);
 	return 0;
 }
