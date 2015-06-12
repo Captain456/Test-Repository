4f3e9e618eff8eb541225656db93a4cb966afb2e
Chris Munroe <idkm23@gmail.com>
2015-06-12 16:56:29 -0400

    added color tracking

--- image_reader/src/image_reader_node.cpp
+++ image_reader/src/image_reader_node.cpp
@@ -4,10 +4,22 @@
 #include "sensor_msgs/Image.h"
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include "opencv2/imgproc/imgproc.hpp"
 
 using namespace cv;
 
+int iLowH = 170,
+    iHighH = 179,
+    iLowS = 150,
+    iHighS = 255,
+    iLowV = 60,
+    iHighV = 255;
+
+
 void call_back(const sensor_msgs::ImageConstPtr& msg) {
+
+    vector< vector<Point> > contours;
+    vector<Vec4i> hierarchy;
 
     cv_bridge::CvImagePtr cv_ptr;
     try
@@ -19,7 +31,55 @@ void call_back(const sensor_msgs::ImageConstPtr& msg) {
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
+
+        //Create trackbars in "Control" window
+    cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+
+    cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+
+    cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+    cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+
+    Mat imgHSV;
+
+    cvtColor(cv_ptr->image, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+    Mat imgThresholded;
+
+    inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); 
+    //Threshold the image
+      
+    //morphological opening (remove small objects from the foreground)
+    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+
+    //morphological closing (fill small holes in the foreground)
+    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
     
+    //Calculate the moments of the thresholded image
+    Moments oMoments = moments(imgThresholded);
+
+    double dM01 = oMoments.m01;
+    double dM10 = oMoments.m10;
+    double dArea = oMoments.m00;
+
+    // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+    if (dArea > 10000)
+    {
+        //calculate the position of the ball
+        int posX = dM10 / dArea;
+        int posY = dM01 / dArea;        
+        
+        line(cv_ptr->image, Point(posX, posY), Point(0, 0), Scalar(0,0,255), 2);
+    }
+    namedWindow("Control", CV_WINDOW_AUTOSIZE);
+    namedWindow("Thresholded Image", CV_WINDOW_AUTOSIZE);
+    imshow("Thresholded Image", imgThresholded); //show the thresholded image
+
     namedWindow("please", CV_WINDOW_AUTOSIZE);
     imshow("please", cv_ptr->image);
     waitKey(5); 
